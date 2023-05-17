136b9b3cb35ec0b7c926a5df87210ae99600f0f2
Evgenyi <evgenyi.logachev@gmail.com>
2023-05-18 01:50:26 +0300

    170523

--- tiffcountoursfiltersnumbersclusterspsfresultsecond.py
+++ tiffcountoursfiltersnumbersclusterspsfresultsecond.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[ ]:
 
 
 from PIL import Image
@@ -337,10 +337,4 @@ def main():
     
 if __name__ == "__main__":
     main()
-
-
-# In[ ]:
-
-
-
 
