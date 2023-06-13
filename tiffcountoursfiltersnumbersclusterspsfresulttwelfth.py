802a55b8c275cc3f9a337a25d2955268eb854f52
Evgenyi <evgenyi.logachev@gmail.com>
2023-06-14 01:34:27 +0300

    140623

--- tiffcountoursfiltersnumbersclusterspsfresulttwelfth.py
+++ tiffcountoursfiltersnumbersclusterspsfresulttwelfth.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
@@ -40,6 +46,132 @@ from skimage.metrics import peak_signal_noise_ratio
 import pandas as pd
 from skimage.filters.rank import entropy
 from skimage.morphology import disk
+
+
+from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
+from matplotlib.lines import Line2D
+
+import numpy as np
+
+def boxplot_2d(x,y, ax, whis=1.5):
+    xlimits = [np.percentile(x, q) for q in (25, 50, 75)]
+    ylimits = [np.percentile(y, q) for q in (25, 50, 75)]
+
+    ##the box
+    box = Rectangle(
+        (xlimits[0],ylimits[0]),
+        (xlimits[2]-xlimits[0]),
+        (ylimits[2]-ylimits[0]),
+        ec = 'k',
+        zorder=0
+    )
+    ax.add_patch(box)
+
+    ##the x median
+    vline = Line2D(
+        [xlimits[1],xlimits[1]],[ylimits[0],ylimits[2]],
+        color='k',
+        zorder=1
+    )
+    ax.add_line(vline)
+
+    ##the y median
+    hline = Line2D(
+        [xlimits[0],xlimits[2]],[ylimits[1],ylimits[1]],
+        color='k',
+        zorder=1
+    )
+    ax.add_line(hline)
+
+    ##the central point
+    ax.plot([xlimits[1]],[ylimits[1]], color='k', marker='o')
+
+    ##the x-whisker
+    ##defined as in matplotlib boxplot:
+    ##As a float, determines the reach of the whiskers to the beyond the
+    ##first and third quartiles. In other words, where IQR is the
+    ##interquartile range (Q3-Q1), the upper whisker will extend to
+    ##last datum less than Q3 + whis*IQR). Similarly, the lower whisker
+    ####will extend to the first datum greater than Q1 - whis*IQR. Beyond
+    ##the whiskers, data are considered outliers and are plotted as
+    ##individual points. Set this to an unreasonably high value to force
+    ##the whiskers to show the min and max values. Alternatively, set this
+    ##to an ascending sequence of percentile (e.g., [5, 95]) to set the
+    ##whiskers at specific percentiles of the data. Finally, whis can
+    ##be the string 'range' to force the whiskers to the min and max of
+    ##the data.
+    iqr = xlimits[2]-xlimits[0]
+
+    ##left
+    left = np.min(x[x > xlimits[0]-whis*iqr])
+    whisker_line = Line2D(
+        [left, xlimits[0]], [ylimits[1],ylimits[1]],
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_line)
+    whisker_bar = Line2D(
+        [left, left], [ylimits[0],ylimits[2]],
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_bar)
+
+    ##right
+    right = np.max(x[x < xlimits[2]+whis*iqr])
+    whisker_line = Line2D(
+        [right, xlimits[2]], [ylimits[1],ylimits[1]],
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_line)
+    whisker_bar = Line2D(
+        [right, right], [ylimits[0],ylimits[2]],
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_bar)
+
+    ##the y-whisker
+    iqr = ylimits[2]-ylimits[0]
+
+    ##bottom
+    bottom = np.min(y[y > ylimits[0]-whis*iqr])
+    whisker_line = Line2D(
+        [xlimits[1],xlimits[1]], [bottom, ylimits[0]], 
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_line)
+    whisker_bar = Line2D(
+        [xlimits[0],xlimits[2]], [bottom, bottom], 
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_bar)
+
+    ##top
+    top = np.max(y[y < ylimits[2]+whis*iqr])
+    whisker_line = Line2D(
+        [xlimits[1],xlimits[1]], [top, ylimits[2]], 
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_line)
+    whisker_bar = Line2D(
+        [xlimits[0],xlimits[2]], [top, top], 
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_bar)
+
+    ##outliers
+    mask = (x<left)|(x>right)|(y<bottom)|(y>top)
+    ax.scatter(
+        x[mask],y[mask],
+        facecolors='none', edgecolors='k'
+    )
 
 
 
@@ -606,7 +738,7 @@ def countourfind(image):
         image=eng.imcontour(imagesource,1)
     except:
         pass
-    eng.quit()''
+    eng.quit()
     print('Quality')
     xcentr=np.asarray(xcentr,dtype=float)
     print(-10*np.log10(special.erfc(xcentr.mean()/xcentr.std())))
@@ -625,6 +757,14 @@ def countourfind(image):
     
 def main():
     image=readimage("C:/Users/evgen/Downloads/s_1_1102_c.jpg")
+    fig,(ax1,ax2) = plt.subplots(ncols=2)
+    ax1.imshow(image,cmap='gray',vmax=image.max(),vmin=image.min())
+    ax1.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    boxplot_2d(image[0:int(image.shape[0]),:],image[:,0:int(image.shape[1])],ax=ax2, whis=1.5)
+    ax2.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    
     #image=readimage("C:/s_1_1102_c.jpg")
     #image=readimage("C:/s_1_1101_a.jpg")
     #image=image[0:1000,0:1000]
@@ -683,6 +823,13 @@ def main():
     
     image=filtration(image,"C:/Users/evgen/Downloads/s_1_1101_a.jpg")
     #image=filtration(image,"C:/s_1_1101_a.jpg")
+    fig,(ax1,ax2) = plt.subplots(ncols=2)
+    ax1.imshow(image,cmap='gray',vmax=image.max(),vmin=image.min())
+    ax1.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    boxplot_2d(image[0:int(image.shape[0]),:],image[:,0:int(image.shape[1])],ax=ax2, whis=1.5)
+    ax2.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
     
     plt.figure(figsize=(15,7))
     plt.imshow(image[0:1000,0:1000],cmap='gray',vmax=image.max(),vmin=image.min())
@@ -697,3 +844,10 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+
+# In[ ]:
+
+
+
+
