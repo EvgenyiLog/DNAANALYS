2c0ac4ba9f0527c52f63a490a4f82f735e5e3a27
Evgenyi <evgenyi.logachev@gmail.com>
2023-09-14 01:47:50 +0300

    130923

---
+++ pixelclusters.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from matplotlib.lines import Line2D
+
+
+def boxplot_2d(x,y, ax, whis=1.5):
+    xlimits = [np.percentile(x, q) for q in (25, 50, 75)]
+    ylimits = [np.percentile(y, q) for q in (25, 50, 75)]
+
+    ##the box
+    box = Rectangle(
+        (xlimits[0],ylimits[0]),
+        (xlimits[2]-xlimits[0]),
+        (ylimits[2]-ylimits[0]),
+        ec = 'k',
+        zorder=0
+    )
+    ax.add_patch(box)
+
+    ##the x median
+    vline = Line2D(
+        [xlimits[1],xlimits[1]],[ylimits[0],ylimits[2]],
+        color='k',
+        zorder=1
+    )
+    ax.add_line(vline)
+
+    ##the y median
+    hline = Line2D(
+        [xlimits[0],xlimits[2]],[ylimits[1],ylimits[1]],
+        color='k',
+        zorder=1
+    )
+    ax.add_line(hline)
+
+    ##the central point
+    ax.plot([xlimits[1]],[ylimits[1]], color='k', marker='o')
+
+    ##the x-whisker
+    ##defined as in matplotlib boxplot:
+    ##As a float, determines the reach of the whiskers to the beyond the
+    ##first and third quartiles. In other words, where IQR is the
+    ##interquartile range (Q3-Q1), the upper whisker will extend to
+    ##last datum less than Q3 + whis*IQR). Similarly, the lower whisker
+    ####will extend to the first datum greater than Q1 - whis*IQR. Beyond
+    ##the whiskers, data are considered outliers and are plotted as
+    ##individual points. Set this to an unreasonably high value to force
+    ##the whiskers to show the min and max values. Alternatively, set this
+    ##to an ascending sequence of percentile (e.g., [5, 95]) to set the
+    ##whiskers at specific percentiles of the data. Finally, whis can
+    ##be the string 'range' to force the whiskers to the min and max of
+    ##the data.
+    iqr = xlimits[2]-xlimits[0]
+
+    ##left
+    left = np.min(x[x > xlimits[0]-whis*iqr])
+    whisker_line = Line2D(
+        [left, xlimits[0]], [ylimits[1],ylimits[1]],
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_line)
+    whisker_bar = Line2D(
+        [left, left], [ylimits[0],ylimits[2]],
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_bar)
+
+    ##right
+    right = np.max(x[x < xlimits[2]+whis*iqr])
+    whisker_line = Line2D(
+        [right, xlimits[2]], [ylimits[1],ylimits[1]],
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_line)
+    whisker_bar = Line2D(
+        [right, right], [ylimits[0],ylimits[2]],
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_bar)
+
+    ##the y-whisker
+    iqr = ylimits[2]-ylimits[0]
+
+    ##bottom
+    bottom = np.min(y[y > ylimits[0]-whis*iqr])
+    whisker_line = Line2D(
+        [xlimits[1],xlimits[1]], [bottom, ylimits[0]], 
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_line)
+    whisker_bar = Line2D(
+        [xlimits[0],xlimits[2]], [bottom, bottom], 
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_bar)
+
+    ##top
+    top = np.max(y[y < ylimits[2]+whis*iqr])
+    whisker_line = Line2D(
+        [xlimits[1],xlimits[1]], [top, ylimits[2]], 
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_line)
+    whisker_bar = Line2D(
+        [xlimits[0],xlimits[2]], [top, top], 
+        color = 'k',
+        zorder = 1
+    )
+    ax.add_line(whisker_bar)
+
+    ##outliers
+    mask = (x<left)|(x>right)|(y<bottom)|(y>top)
+    ax.scatter(
+        x[mask],y[mask],
+        facecolors='none', edgecolors='k'
+    )
+
+
+def readimage(path):
+    'path to file'
+    'чтение файла возвращает изображение'
+    
+    image = cv2.imread(path,0)
+    #print(image.shape)
+    print(image.dtype)
+    return image
+
+def tiffreader(path):
+    image=cv2.imread(path,-1)
+    #print(image.shape)
+    print(image.dtype)
+    return image
+
+def filtration(image):
+    hist = cv2.calcHist([image],[0],None,[4096],[0,4096])
+    print(np.amax(hist))
+    print(np.argmax(hist))
+    #print(type(hist))
+    h=list(hist)
+    index05max=[]
+    for i in h:
+        if i>=0.5*np.amax(hist):
+            index05max.append(h.index(i))
+    index05max=np.asarray(index05max,dtype=np.int64)        
+    #print(index05max.ravel())
+    print(np.amax(index05max))
+    plt.figure()
+    plt.plot(hist)
+    plt.axvline(x=np.argmax(hist), color='b', label='max')
+    plt.axvline(x=np.amax(index05max), color='g', label='max')
+    plt.grid(True)
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    
+    plt.figure()
+    plt.imshow(image[0:500,0:500],cmap='gray',vmax=image.max(),vmin=image.min())
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    
+    fig,(ax1,ax2) = plt.subplots(ncols=2)
+    ax1.imshow(image,cmap='gray',vmax=image.max(),vmin=image.min())
+    ax1.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    boxplot_2d(image[0:int(image.shape[0]),:],image[:,0:int(image.shape[1])],ax=ax2, whis=7)
+    ax2.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    image=np.where(image<np.amax(index05max),image,0)
+    plt.figure()
+    plt.imshow(image[0:100,0:100],cmap='gray',vmax=image.max(),vmin=image.min())
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    print(np.amax(image))
+    print(image.dtype)
+    fig,(ax1,ax2) = plt.subplots(ncols=2)
+    ax1.imshow(image,cmap='gray',vmax=image.max(),vmin=image.min())
+    ax1.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    boxplot_2d(image[0:int(image.shape[0]),:],image[:,0:int(image.shape[1])],ax=ax2, whis=7)
+    ax2.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    plt.figure()
+    plt.imshow(image[0:500,0:500],cmap='gray',vmax=image.max(),vmin=image.min())
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    return image
+
+import skimage
+from scipy import ndimage as ndi
+def clusters(image):
+    x,y=skimage.morphology.local_maxima(image,indices=True)
+    print(len(x))
+    maxima=skimage.morphology.h_maxima(image,np.mean(image))
+    print(len(maxima)/2)
+    coordinates=skimage.feature.peak_local_max(image,min_distance=3)
+    print(coordinates)
+
+
+def main():
+    image=tiffreader("C:/Users/evgen/Downloads/s_1_1102_c.tif")
+    image=filtration(image)
+    clusters(image)
+    plt.show()
+    
+    
+if __name__ == "__main__":
+    main()   
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
