af4226b712c55e9f58cf99099f0bca72e178b7f0
Evgenyi <evgenyi.logachev@gmail.com>
2023-10-26 01:39:20 +0300

    251023

---
+++ modeltreshelboltgt.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy
+from numpy.fft import fft, ifft
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def readimage(path):
+    'path to file'
+    'чтение файла возвращает изображение'
+    
+    image = cv2.imread(path,0)
+    #print(image.shape)
+    h, w = image.shape[:2]
+    print(f'Weigth= {w}')
+    print(f'Heigth= {h}')
+    print(image.dtype)
+    return image
+
+def tiffreader(path):
+    image=cv2.imread(path,-1)
+    #print(image.shape)
+    print(image.dtype)
+    h, w = image.shape[:2]
+    print(f'Weigth= {w}')
+    print(f'Heigth= {h}')
+    return image
+
+def modelimage():
+    N=int(input('Введите высоту изображения= '))
+    M=int(input('Введите ширину изображения= '))
+    K1=int(input('Введите количество точек 1= '))
+    K2=int(input('Введите количество точек 2= '))
+    K3=int(input('Введите количество точек 3= '))
+    K4=int(input('Введите количество точек 4= '))
+    K5=int(input('Введите количество точек 5= '))
+    R1=int(input('Введите радиус 1='))
+    R2=int(input('Введите радиус 2='))
+    R3=int(input('Введите радиус 3='))
+    R4=int(input('Введите радиус 4='))
+    R5=int(input('Введите радиус 5='))
+    image=np.zeros((M,N))
+    print(image.shape)
+    xx, yy = np.ogrid[0:M,0:N]
+    xcentr1=(M+1) * np.random.sample(size=K1)
+    ycentr1=(N+1)* np.random.sample(size=K1)
+    xcentr1=np.ceil(xcentr1)
+    xcentr1=np.asarray(xcentr1,dtype=int)
+    ycentr1=np.floor(ycentr1)
+    ycentr1=np.asarray(ycentr1,dtype=int)
+    #print(ycentr1)
+    #print(xcentr1)
+    xcentr2=(M+1) * np.random.sample(size=K2)
+    ycentr2=(N+1)* np.random.sample(size=K2)
+    xcentr2=np.ceil(xcentr2)
+    xcentr2=np.asarray(xcentr2,dtype=int)
+    ycentr2=np.floor(ycentr2)
+    ycentr2=np.asarray(ycentr2,dtype=int)
+    xcentr3=(M+1) * np.random.sample(size=K3)
+    ycentr3=(N+1)* np.random.sample(size=K3)
+    xcentr3=np.ceil(xcentr3)
+    xcentr3=np.asarray(xcentr3,dtype=int)
+    ycentr3=np.floor(ycentr3)
+    ycentr3=np.asarray(ycentr3,dtype=int)
+    
+    xcentr4=(M+1) * np.random.sample(size=K4)
+    ycentr4=(N+1)* np.random.sample(size=K4)
+    xcentr4=np.ceil(xcentr4)
+    xcentr4=np.asarray(xcentr4,dtype=int)
+    ycentr4=np.floor(ycentr4)
+    ycentr4=np.asarray(ycentr4,dtype=int)
+    
+    xcentr5=(M+1) * np.random.sample(size=K5)
+    ycentr5=(N+1)* np.random.sample(size=K5)
+    xcentr5=np.ceil(xcentr5)
+    xcentr5=np.asarray(xcentr5,dtype=int)
+    ycentr5=np.floor(ycentr5)
+    ycentr5=np.asarray(ycentr5,dtype=int)                   
+    
+    image=np.asarray(image,dtype=np.uint8)
+    
+    for i in range(K1):
+        imagem=cv2.circle(image, (xcentr1[i], ycentr1[i]), R1, (255, 255, 255), -1)
+    for i in range(K2):
+        imagem=cv2.circle(imagem, (xcentr2[i], ycentr2[i]), R2, (200, 200, 200), -1)
+    for i in range(K3):
+        imagem=cv2.circle(imagem, (xcentr3[i], ycentr3[i]), R3, (240, 240, 240), -1)
+        
+    for i in range(K4):
+        imagem=cv2.circle(imagem, (xcentr4[i], ycentr4[i]), R4, (230, 230, 230), -1)
+        
+    for i in range(K5):
+        imagem=cv2.circle(imagem, (xcentr5[i], ycentr5[i]), R5, (220, 220, 220), -1)
+        
+        
+    for i in range(K1):
+        imagem=cv2.circle(image, (xcentr1[i], ycentr1[i]), 1, (255, 0, 0), -1)
+    for i in range(K2):
+        imagem=cv2.circle(imagem, (xcentr2[i], ycentr2[i]), 1, (255, 0, 0), -1)
+    for i in range(K3):
+        imagem=cv2.circle(imagem, (xcentr3[i], ycentr3[i]), 1, (255, 0, 0), -1)
+        
+    for i in range(K4):
+        imagem=cv2.circle(imagem, (xcentr4[i], ycentr4[i]), 1, (255, 0, 0), -1)
+        
+    for i in range(K5):
+        imagem=cv2.circle(imagem, (xcentr5[i], ycentr5[i]), 1, (255, 0, 0), -1)
+        
+    imagem=cv2.normalize(imagem, None, 0, 4096, cv2.NORM_MINMAX, dtype=cv2.CV_16U)
+        
+    imagem= cv2.blur(imagem,(3,3))
+    imagem = cv2.GaussianBlur(imagem, (3,3),0)
+    kernel = np.ones((3,3),np.float32)/9
+    imagem = cv2.filter2D(imagem,-1,kernel)
+    imagem = cv2.medianBlur(imagem,3)
+    imagem = cv2.boxFilter(imagem, -1, (3,3))
+    
+    print(imagem.dtype)
+        
+        
+    plt.figure(figsize=(15, 7))
+    plt.imshow(imagem, cmap=plt.cm.gray,vmax=imagem.max(),vmin=imagem.min())
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+    
+    
+    
+    
+    
+    fig = plt.figure(figsize=(15,7))          #create a canvas, tell matplotlib it's 3d                                                                                                              fig = plt.figure(figsize=(15,7))          #create a canvas, tell matplotlib it's 3d
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(xx,yy,imagem)
+    ax.grid(True)
+    return imagem
+
+def main():
+    image=modelimage()
+    plt.figure(figsize=(15, 7))
+    plt.imshow(image, cmap=plt.cm.gray,vmax=image.max(),vmin=image.min())
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений 
+    ret,thresh1 = cv2.threshold(image,0,4096,cv2.THRESH_BINARY)
+    ret,thresh2 = cv2.threshold(image,0,4096,cv2.THRESH_BINARY_INV)
+    ret,thresh3 = cv2.threshold(image,0,4096,cv2.THRESH_TRUNC)
+    ret,thresh4 = cv2.threshold(image,0,4096,cv2.THRESH_TOZERO)
+    ret,thresh5 = cv2.threshold(image,0,4096,cv2.THRESH_TOZERO_INV)
+    ret,thresh6 = cv2.threshold(image,0,4096,cv2.THRESH_OTSU)
+    titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV','OTSU']
+    images = [image, thresh1, thresh2, thresh3, thresh4, thresh5,thresh6]
+    for i in range(7):
+        plt.subplot(1,7,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=4096)
+        plt.title(titles[i])
+        plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+        
+    image=cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    ret,th1 = cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    try:
+        th2 = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+                                    cv2.THRESH_BINARY+cv2.THRESH_OTSU,3,5)
+        print('sucsessfull mean')
+    except:
+        th2=th1
+    try:
+        th3 = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                                    cv2.THRESH_BINARY+cv2.THRESH_OTSU,3,5)
+        print('sucsessfull gauss')
+    except:
+        th3=th2
+    titles = ['Original Image', 'Global Thresholding (v = 255)',
+    'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+    images = [image, th1, th2, th3]
+    for i in range(4):
+        plt.subplot(2,2,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=4096)
+        plt.title(titles[i]) 
+        plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делений
+        
+        
+    rows,cols = image.shape
+    image=cv2.normalize(image, None, 0, 4096, cv2.NORM_MINMAX, dtype=cv2.CV_16U) 
+    M = np.float32([[1,0,100],[0,1,50]])
+    imagewA = cv2.warpAffine(image,M,(cols,rows))
+    plt.figure(figsize=(15, 7))
+    plt.imshow(imagewA, cmap=plt.cm.gray,vmax=imagewA.max(),vmin=imagewA.min())
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делени
+    
+    M= cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+    dst = cv2.warpAffine(image,M,(cols,rows))
+    plt.figure(figsize=(15, 7))
+    plt.imshow(dst,cmap=plt.cm.gray,vmax=dst.max(),vmin=dst.min())
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делени
+    
+    
+    pts1 = np.float32([[50,50],[200,50],[50,200]])
+    pts2 = np.float32([[10,100],[200,50],[100,250]])
+    
+    M = cv2.getAffineTransform(pts1,pts2)
+     
+    dst = cv2.warpAffine(image,M,(cols,rows))
+    plt.figure(figsize=(15, 7))
+    plt.subplot(121),plt.imshow(image,cmap=plt.cm.gray,vmax=image.max(),vmin=image.min()),plt.title('Input')
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делени
+    plt.subplot(122),plt.imshow(dst,cmap=plt.cm.gray,vmax=dst.max(),vmin=dst.min()),plt.title('Output')
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делени
+    
+    
+    pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+    pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+    
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    
+    dst = cv2.warpPerspective(image,M,(300,300))
+    plt.figure(figsize=(15, 7))
+    plt.subplot(121),plt.imshow(image,cmap=plt.cm.gray,vmax=image.max(),vmin=image.min()),plt.title('Input')
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делени
+    plt.subplot(122),plt.imshow(dst,cmap=plt.cm.gray,vmax=dst.max(),vmin=dst.min()),plt.title('Output')
+    plt.tick_params(labelsize =20,#  Размер подписи
+                    color = 'k')   #  Цвет делени
+   
+
+    plt.show()
+    
+    
+if __name__ == "__main__":
+    main()
+        
+
+
+# In[ ]:
+
+
+
+
